12001b6873a5c8eb532fd2a9de424ba33a35c8cc
GuyTalmi <guytalmi@gmail.com>
2023-04-11 16:17:21 +0300

    Update CartController.

--- Mango.Services.ShoppingCartApi/Controllers/CartController.cs
+++ Mango.Services.ShoppingCartApi/Controllers/CartController.cs
@@ -37,5 +37,59 @@ namespace Mango.Services.ShoppingCartApi.Controllers
 
 			return _response;
 		}
+
+		[HttpPost("AddCart")]
+		public async Task<object> AddCart(CartDto cartDto)
+		{
+			try
+			{
+				CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+				_response.Result = cartDt;
+
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+
+			return _response;
+		}
+
+		[HttpPost("UpdateCart")]
+		public async Task<object> UpdateCart(CartDto cartDto)
+		{
+			try
+			{
+				CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+				_response.Result = cartDt;
+
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+
+			return _response;
+		}
+
+		[HttpPost("RemoveCart")]
+		public async Task<object> RemoveCart([FromBody] int cartId)
+		{
+			try
+			{
+				bool isSuccess = await _cartRepository.RemoveFromCart(cartId);
+				_response.Result = isSuccess;
+
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { ex.ToString() };
+			}
+
+			return _response;
+		}
 	}
 }

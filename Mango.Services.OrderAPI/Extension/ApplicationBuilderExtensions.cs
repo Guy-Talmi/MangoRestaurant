96c7e305220ab535c58503069bbfec5803e6e6c5
GuyTalmi <guytalmi@gmail.com>
2023-04-13 17:29:38 +0300

    Add order API.

---
+++ Mango.Services.OrderAPI/Extension/ApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Services.OrderAPI.Messaging;
+
+namespace Mango.Services.OrderAPI.Extension
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
+
+        public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+        {
+            ServiceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
+            var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            hostApplicationLife.ApplicationStarted.Register(OnStarted);
+            hostApplicationLife.ApplicationStopping.Register(OnStopping);
+
+            return app;
+        }
+
+        private static void OnStarted()
+        {
+            ServiceBusConsumer.Start();
+        }
+
+        private static void OnStopping()
+        {
+            ServiceBusConsumer.Stop();
+        }
+    }
+}
---
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,99 @@
+﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.OrderAPI.Messages;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Repository;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mango.Services.OrderAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string checkoutMessageTopic;
+        private readonly string SubscriptionCheckout;
+
+        private readonly IConfiguration _configuration;
+
+        private readonly OrderRepository _orderRepository;
+
+        private ServiceBusProcessor checkOutProcessor;
+
+        public AzureServiceBusConsumer(OrderRepository orderRepository, IConfiguration configuration)
+        {
+            _orderRepository = orderRepository;
+            _configuration = configuration;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+            SubscriptionCheckout = _configuration.GetValue<string>("SubscriptionCheckout");
+            checkoutMessageTopic = _configuration.GetValue<string>("CheckoutMessageTopic");
+
+            var client = new ServiceBusClient(serviceBusConnectionString);
+
+            checkOutProcessor = client.CreateProcessor(checkoutMessageTopic, SubscriptionCheckout);
+        }
+
+        public async Task Start()
+        {
+            checkOutProcessor.ProcessMessageAsync += OnCheckOutMessageReceived;
+            checkOutProcessor.ProcessErrorAsync += OnErrorHandler;
+
+            await checkOutProcessor.StartProcessingAsync();
+        }
+
+        public async Task Stop()
+        {
+            await checkOutProcessor.StopProcessingAsync();
+            await checkOutProcessor.DisposeAsync();
+        }
+
+        private Task OnErrorHandler(ProcessErrorEventArgs arg)
+        {
+            Console.WriteLine(arg.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDto.UserId,
+                FirstName = checkoutHeaderDto.FirstName,
+                LastName = checkoutHeaderDto.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CardNumber = checkoutHeaderDto.CardNumber,
+                CouponCode = checkoutHeaderDto.CouponCode,
+                CVV = checkoutHeaderDto.CVV,
+                DiscountTotal = checkoutHeaderDto.DiscountTotal,
+                Email = checkoutHeaderDto.Email,
+                ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDto.OrderTotal,
+                PaymentStatus = false,
+                Phone = checkoutHeaderDto.Phone,
+                PickupDateTime = checkoutHeaderDto.PickupDateTime
+            };
+
+            foreach (var detailList in checkoutHeaderDto.CartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detailList.ProductId,
+                    ProductName = detailList.Product.Name,
+                    Price = detailList.Product.Price,
+                    Count = detailList.Count
+                };
+
+                orderHeader.CartTotalItems += detailList.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+
+            await _orderRepository.AddOrder(orderHeader);
+        }
+    }
+}
